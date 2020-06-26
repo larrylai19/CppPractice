9f921d06e2feb11b8180a9bfa9a28f5bf9913436
minyuan1119 <stji9gjz2@gmail.com>
2020-06-27 02:40:38 +0800

    Update 11417.cpp

--- UVa/11417.cpp
+++ UVa/11417.cpp
@@ -4,24 +4,24 @@ using namespace std;
 
 int GCD(int i, int j)
 {
-	if (j % i == 0)
-		return i;
-	else
-		GCD(j % i, i);
+    if (j % i == 0)
+        return i;
+    else
+        GCD(j % i, i);
 }
 
 int main()
 {
-	int n, G;
+    int n, G;
 
-	while ((cin >> n) && (n != 0))
-	{
-		G = 0;
+    while ((cin >> n) && (n != 0))
+    {
+        G = 0;
 
-		for (int i = 1; i < n; i++)
-			for (int j = i + 1; j <= n; j++)
-				G += GCD(i, j);
+        for (int i = 1; i < n; i++)
+            for (int j = i + 1; j <= n; j++)
+                G += GCD(i, j);
 
-		cout << G << endl;
-	}
+        cout << G << endl;
+    }
 }
