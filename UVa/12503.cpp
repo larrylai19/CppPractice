1ae2e0450fe7ac812f7cfa7dd2f353ab313267dd
minyuan1119 <stji9gjz2@gmail.com>
2020-06-28 00:05:11 +0800

    Create 12503.cpp

---
+++ UVa/12503.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    int T, p, temp, moveNum;
+    string move;
+    vector< int > moveTemp;
+
+    cin >> T;
+
+    while (T--)
+    {
+        p = 0;
+
+        cin >> moveNum;
+        moveTemp.clear();
+
+        while (moveNum--)
+        {
+            cin >> move;
+
+            switch (move[0])
+            {
+            case 'L':
+                --p;
+                moveTemp.push_back(-1);
+                break;
+            case 'R':
+                ++p;
+                moveTemp.push_back(1);
+                break;
+            case 'S':
+                cin >> move >> temp;
+                p += moveTemp[temp - 1];
+                moveTemp.push_back(moveTemp[temp - 1]);
+                break;
+            }
+        }
+
+        cout << p << endl;
+    }
+}
