c501d63f6b977be9a698a7704cfb5b8f14d1e9bc
minyuan1119 <stji9gjz2@gmail.com>
2020-07-07 21:02:40 +0800

    Create 53.cpp

---
+++ LeetCode/53.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int max = nums[0]; // ¦³¥i¯à nums.size() = 1    
+        for(size_t i = 0; i < nums.size(); ++i)
+        {
+            int sum = 0;
+            for(size_t j = i; j < nums.size(); ++j)
+            {
+                sum += nums[j];
+                if(sum > max)
+                    max = sum;
+            }   
+        }
+        return max;
+    }
+};
