4f475f1c2027381853c20756517d78dfe985c749
minyuan1119 <stji9gjz2@gmail.com>
2020-07-08 21:32:56 +0800

    leetcode 876

---
+++ LeetCode/876-1.cpp
@@ -0,0 +1,20 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        int length = 0;
+        auto ans = head;
+        for(auto it = head; it != nullptr; it = it->next, ++length);
+        for(int i = 0; i < length / 2; ++i, ans = ans->next);
+        return ans;
+    }
+};
