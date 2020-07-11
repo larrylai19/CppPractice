0bb2fa32f987b972709ae5732835a60364026785
minyuan1119 <stji9gjz2@gmail.com>
2020-07-11 22:59:36 +0800

    Update 238-2.cpp

--- LeetCode/238-2.cpp
+++ LeetCode/238-2.cpp
@@ -5,8 +5,8 @@ public:
         vector<int> rightProduct(n, 1);
         for(int i = n - 2; i >= 0; --i)
             rightProduct[i] = rightProduct[i + 1] * nums[i + 1];
-        for(int i = 0, left = 1; i < n; ++i)
-            rightProduct[i] *= left,    left *= nums[i];
+        for(int i = 0, left = 1; i < n; left *= nums[i], ++i)
+            rightProduct[i] *= left;
         return rightProduct;
     }
 };
