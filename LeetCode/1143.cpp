63c734e19673fbfea32362804e8df0d3c7b7d15b
minyuan1119 <stji9gjz2@gmail.com>
2020-07-20 22:15:26 +0800

    Update 1143.cpp

--- LeetCode/1143.cpp
+++ LeetCode/1143.cpp
@@ -2,15 +2,12 @@ class Solution {
 public:
     int longestCommonSubsequence(string text1, string text2) {
         int n1 = text1.size(), n2 = text2.size();
-        // 使 index 從 1 開始，這樣 dp 可以不用檢查邊界條件
-        text1 = "#" + text1;
-        text2 = "#" + text2;
-        // dp[i][j] = text1[i], text2[j] 中的 LCS
+        // dp[i][j] = text1[i], text2[j] 的 LCS
         auto dp = vector<vector<int>>(n1 + 1, vector<int>(n2 + 1, 0));
         for(int i = 1; i <= n1; ++i)
             for(int j = 1; j <= n2; ++j)
                 // 如果兩者相等，最大長度為兩者 - 1 的最大長度 + 1
-                if(text1[i] == text2[j])
+                if(text1[i - 1] == text2[j - 1])
                     dp[i][j] = dp[i - 1][j - 1] + 1;
                 // 否則為兩者其中一者 - 1 的最大值
                 else
