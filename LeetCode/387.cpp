ef8c774406a57cd42d3692cb78d890cfcd5c7ff1
minyuan1119 <stji9gjz2@gmail.com>
2020-07-24 20:06:16 +0800

    Create 387.cpp

---
+++ LeetCode/387.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        int n = s.size();
+        int count[26] = {};
+        for(auto i : s)
+            ++count[i - 'a'];
+        for(int i = 0; i < n; ++i)
+            if(count[s[i] - 'a'] == 1)
+                return i;
+        return -1;
+    }
+};
