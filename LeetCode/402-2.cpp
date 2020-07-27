f581dd45d9c856849ef24909a00266d24416afd4
minyuan1119 <stji9gjz2@gmail.com>
2020-07-27 20:39:39 +0800

    leetcode 402

---
+++ LeetCode/402-2.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string removeKdigits(string num, int k) {
+        int n = num.size();
+        if(n == k)
+            return "0";
+        int digits = n - k;
+        string res;
+        for(auto i : num)
+        {
+            while(k && !res.empty() && res.back() > i)
+                res.pop_back(), --k;
+            if(!res.empty() || i != '0')
+                res.push_back(i);
+        }
+        while(k--)
+            res.pop_back();
+        return res.empty() ? "0" : res;
+    }
+};
---
+++ LeetCode/402-3.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string removeKdigits(string num, int k) {
+        int n = num.size();
+        if(n == k)
+            return "0";
+        int digits = n - k, index = 0;
+        for(auto i : num)
+        {
+            while(k && index && num[index - 1] > i)
+                --index, --k;
+            if(index || i != '0')
+                num[index++] = i;
+        }
+        while(k--)
+            --index;
+        num.erase(index);
+        return num.empty() ? "0" : num;
+    }
+};
