90a4d5845af587d8c58b9e9f267ce0bfa69d14f9
minyuan1119 <stji9gjz2@gmail.com>
2020-08-19 21:10:49 +0800

    Update 824.cpp

--- LeetCode/824.cpp
+++ LeetCode/824.cpp
@@ -2,15 +2,14 @@ class Solution {
 public:
     string toGoatLatin(string S) {
         stringstream ss(S);
-        string tmp, res, _append = "maa";
+        string tmp, res, _append = "ma";
         while(ss >> tmp)
         {
             if(!isVowel(tolower(tmp[0]))) tmp += tmp[0], tmp.erase(tmp.begin());
-            res += tmp + plusa(_append) + " ";
+            res += tmp + (_append += 'a') + ' ';
         }
         return res.substr(0, res.size() - 1);
     }
 private:
     inline bool isVowel(char c) { return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u'; }
-    inline string plusa(string& s) { string tmp = s; s += "a"; return tmp; }
 };
