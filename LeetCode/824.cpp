30dd8cb2953ac01652f962a4559882b1184c097d
minyuan1119 <stji9gjz2@gmail.com>
2020-08-19 21:03:11 +0800

    Update 824.cpp

--- LeetCode/824.cpp
+++ LeetCode/824.cpp
@@ -6,11 +6,11 @@ public:
         while(ss >> tmp)
         {
             if(!isVowel(tolower(tmp[0]))) tmp += tmp[0], tmp.erase(tmp.begin());
-            res += tmp + _append + " ";
-            _append += 'a';
+            res += tmp + plusa(_append) + " ";
         }
         return res.substr(0, res.size() - 1);
     }
 private:
-    bool isVowel(char c) { return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u'; }
+    inline bool isVowel(char c) { return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u'; }
+    inline string plusa(string& s) { string tmp = s; s += "a"; return tmp; }
 };
