93b6a443d3d8ba4a620c65cf4ad02cb2d4dac528
minyuan1119 <stji9gjz2@gmail.com>
2020-09-12 11:26:07 +0800

    .

--- LeetCode/53.cpp
+++ LeetCode/53.cpp
@@ -1,17 +1,13 @@
 class Solution {
 public:
-    int maxSubArray(vector<int>& nums) {
-        int max = nums[0]; // 有可能 nums.size() = 1
-        for(size_t i = 0; i < nums.size(); ++i)
+    int maxSubArray(vector<int>& nums)
+    {
+        int mx = nums[0], res = mx, n = nums.size();
+        for(int i = 1; i < n; ++i)
         {
-            int sum = 0;
-            for(size_t j = i; j < nums.size(); ++j)
-            {
-                sum += nums[j];
-                if(sum > max)
-                    max = sum;
-            }   
+            mx = max(mx + nums[i], nums[i]);
+            res = max(res, mx);
         }
-        return max;
+        return res;
     }
 };
