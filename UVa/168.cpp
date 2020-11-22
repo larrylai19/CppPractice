73633daf8dd5eb30dbdbc9edee39509ec66a12e8
minyuan1119 <stji9gjz2@gmail.com>
2020-11-23 00:07:15 +0800

    Update 168.cpp

--- UVa/168.cpp
+++ UVa/168.cpp
@@ -3,7 +3,6 @@
 #include <vector>
 #include <string>
 
-#define LENGTH(str) int(str.length())
 #define SIZE(c) int(c.size())
 #define diff(ch) (ch - 'A')
 
@@ -22,7 +21,7 @@ static auto __ = []
 int main()
 {
     string str;
-    
+
     while (cin >> str && str != "#")
     {
         int k;
@@ -30,32 +29,32 @@ int main()
         cin >> m >> t >> k;
 
         // 讀取資料並建立圖
-        vector<vector<char>> G(26);
-        auto read = [&](int i, int j) { return str[j] != ';' && str[j] != '.'; };
-        for (int i = 0; i < LENGTH(str);)
+        vector<int> G[26];
+        auto read = [&](int j) { return str[j] != ';' && str[j] != '.'; };
+        for (int i = 0; i < SIZE(str);)
         {
             if (str[i] == ':')
             {
                 int j = i + 1;
-                while (read(i, j)) G[diff(str[i - 1])].push_back(str[j++]);
+                while (read(j)) G[diff(str[i - 1])].push_back(diff(str[j++]));
                 i = j + 1;
             }
             else ++i;
         }
 
         int steps = 0;
-        vector<int> candle(26);
+        int candle[26] = {};
 
-        function<void(char, char)> dfs = [&](char M, char T)
+        function<void(int, int)> dfs = [&](int M, int T)
         {
             // 放蠟燭
-            if (steps && steps % k == 0) cout << T << " ", candle[diff(T)] = 1;
+            if (steps && steps % k == 0) cout << char(T + 'A') << " ", candle[T] = 1;
 
-            auto& v = G[diff(M)];
+            auto& v = G[M];
             for (int i = 0; i < SIZE(v); ++i)
             {
                 // 如果 v[i] 沒有蠟燭且 t 不在 v[i] 那 M 就可以往 V[i] 走
-                if (v[i] != T && !candle[diff(v[i])])
+                if (v[i] != T && !candle[v[i]])
                 {
                     ++steps;
                     dfs(v[i], M);
@@ -64,9 +63,9 @@ int main()
             }
 
             // 如果 m 無路可走，代表會在 M 被抓到
-            cout << '/' << M << '\n';
+            cout << '/' << char(M + 'A') << '\n';
         };
 
-        dfs(m, t);
+        dfs(m - 'A', t - 'A');
     }
 }
